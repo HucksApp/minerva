f5e687ec43cce409ba530aaa22e10e42a92f2862
HucksApp <jakehunt000@gmail.com>
2020-09-11 04:30:35 +0100

    add 1

--- minervaMail.mjs
+++ minervaMail.mjs
@@ -1,5 +1,9 @@
 import dotenv from 'dotenv'
 import  Sequelize from 'sequelize'
+import express from 'express'   
+
+
+
 import { connectDbUsingSequelize }  from './db/connectDb.mjs'
 import { runDbConnectTest } from './tests/dbTests.mjs'
 import  Inbox  from './db/Model/inboxModel.mjs'
@@ -7,8 +11,7 @@ import  Inbox  from './db/Model/inboxModel.mjs'
 
 dotenv.config()
 
-
-
+const PORT = 3000;
 
 
 
@@ -18,6 +21,37 @@ const db =  connectDbUsingSequelize(  Sequelize, "postgres", process.env  )
 runDbConnectTest( db ).then(()=>{
 
     Inbox.startInbox ( Inbox,db,Sequelize )
+
+    const minervaMail = express();
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+minervaMail.listen(PORT, ()=> console.log(`MINERVAL_MAIL LISTENING ON ${PORT}`))
+
 
 }).catch(err => console.log( err ))
 
